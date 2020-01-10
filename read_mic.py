cfd39c230cf3b1fe16136d6e8731ecb37abac9df
kel89 <kel89@cornell.edu>
2020-01-09 19:41:56 -0500

    updated read_mic to read for 10 second and show peaks

--- read_mic.py
+++ read_mic.py
@@ -20,19 +20,35 @@ stream = p.open(format=pyaudio.paInt16,
 				)
 
 # create a numpy array holding a single read of audio data
-for i in range(10): #to it a few times just to see
+# for i in range(10): #to it a few times just to see
+# 	data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+# 	print(data)
+# 	print(data.shape)
+
+
+
+
+# # close the stream gracefully
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
+
+# # Plot the last bit of data
+# plt.plot(range(data.shape[0]), data)
+# plt.show()
+
+
+################################################################################
+# Push the envelope
+T = 10 # seconds -- how long we want to run
+r = int(T * (RATE / CHUNK))
+
+for i in range(r):
 	data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-	print(data)
-	print(data.shape)
+	peak=np.average(np.abs(data))*2
+	bars="#"*int(50*peak/2**16)
+	print("%04d %05d %s"%(i,peak,bars))
 
-
-
-
-# close the stream gracefully
 stream.stop_stream()
 stream.close()
 p.terminate()
-
-# Plot the last bit of data
-plt.plot(range(data.shape[0]), data)
-plt.show()
